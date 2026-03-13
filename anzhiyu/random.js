a78c53b9c2ee242c836782438a3fe94789274507
lanren1121 <lanren1121@gmail.com>
2026-03-13 20:04:35 +0800

    Site updated: 2026-03-13 20:04:34

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/03/13/Docker/","2026/03/13/hello-world/"];function toRandomPost(){
+var posts=["2026/03/13/Docker/","2026/03/13/Maybatis-Plus/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
